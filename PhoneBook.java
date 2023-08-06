5db085a125ed5e2adf40fa6eda1c4092504172ff
LiveGobe <pax01012000@gmail.com>
2023-08-06 12:31:29 +1000

    Update PhoneBook.java

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class PhoneBook {
@@ -31,13 +34,83 @@ public class PhoneBook {
         }
     }
 
+    public static void printContactsSortedByName() {
+        ArrayList<Map.Entry<String, ArrayList<String>>> list = new ArrayList(contacts.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String, ArrayList<String>>>() {
+            @Override
+            public int compare(Map.Entry<String, ArrayList<String>> o1, Map.Entry<String, ArrayList<String>> o2) {
+                return o1.getKey().compareTo(o2.getKey());
+            }
+        });
+
+        for (Map.Entry<String, ArrayList<String>> entry : list) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    public static void saveContactsToFile(String filename) {
+        try {
+            FileWriter fileWriter = new FileWriter(filename);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            ArrayList<Map.Entry<String, ArrayList<String>>> list = new ArrayList(contacts.entrySet());
+
+            for (Map.Entry<String, ArrayList<String>> entry : list) {
+                bufferedWriter.write(entry.getKey() + ": " + entry.getValue() + "\n");
+            }
+
+            bufferedWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to file: " + e.getMessage());
+        }
+    }
+
+    public static void searchContact(String name) {
+        if (contacts.containsKey(name)) {
+            System.out.println(name + ": " + contacts.get(name));
+        } else {
+            System.out.println("No contact found for " + name);
+        }
+    }
+
     public static void main(String[] args) {
-        addContact("Kafka", "123-45-67");
-        addContact("Blade", "111-11-11");
-        addContact("Blade", "222-22-22");
-        addContact("Silver Wolf", "333-33-33");
-        addContact("Trailblazer", "987-65-43");
-        
-        printContacts();
+        Scanner scanner = new Scanner(System.in);
+        String action;
+
+        while (true) {
+            System.out.println("\nPlease choose an action: add, print, printSorted, save, search, or quit");
+            action = scanner.nextLine();
+
+            switch (action) {
+                case "add":
+                    System.out.println("Enter name:");
+                    String name = scanner.nextLine();
+                    System.out.println("Enter phone number:");
+                    String phoneNumber = scanner.nextLine();
+                    addContact(name, phoneNumber);
+                    break;
+                case "print":
+                    printContacts();
+                    break;
+                case "printSorted":
+                    printContactsSortedByName();
+                    break;
+                case "save":
+                    System.out.println("Enter filename:");
+                    String filename = scanner.nextLine();
+                    saveContactsToFile(filename);
+                    break;
+                case "search":
+                    System.out.println("Enter name:");
+                    String searchName = scanner.nextLine();
+                    searchContact(searchName);
+                    break;
+                case "quit":
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid action. Please try again.");
+            }
+        }
     }
 }
